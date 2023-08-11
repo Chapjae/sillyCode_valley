666c89dafac4c91ce3830a7568278fa1a8f550f9
kayla <kayladatte@gmail.com>
2023-08-11 13:16:16 -0700

    model relationships completed

--- models/Video.js
+++ models/Video.js
@@ -2,6 +2,9 @@ const { Model, DataTypes } = require("sequelize");
 const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection");
 
+class Video extends Model {}
+
+
 Video.init(
   {
     id: {
