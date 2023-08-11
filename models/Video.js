e32447edbe8626499a2d31308b007db2e2de1580
kayla <kayladatte@gmail.com>
2023-08-11 12:09:01 -0700

    created Video model

---
+++ models/Video.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+
+Video.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    created_on: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    link: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "video",
+  }
+);
+
+module.exports = Video;
