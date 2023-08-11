6332a92b12fb8ec5f0df19863db9f4bc7da1d392
Andy Godoy <Andygodoy@ymail.com>
2023-08-11 16:07:53 -0700

    adjusted the comment routes with the team

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,61 +1,124 @@
 const router = require("express").Router();
+// required the models here
+const { Comment, User } = require('../../models');
 
 // Sample array to simulate a database of comments
 let comments = [];
 
 // Route to get all comments
-router.get("/", (req, res) => {
-  res.json(comments);
+router.get("/", async (req, res) => {
+  try {
+    const commentData = await Comment.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+    // Sending the fetched comment data instead of "comments"
+    res.status(200).json(commentData); 
+  } catch (error) {
+    console.error("Error fetching comments:", error);
+    res.status(500).json({ error: "Error fetching comments." });
+  }
 });
+
 
 // Route to create a new comment
-router.post("/", (req, res) => {
-  const { text } = req.body;
-  if (text) {
-    const newComment = {
-      id: comments.length + 1,
-      text: text
-    };
-    comments.push(newComment);
-    res.status(201).json(newComment);
-  } else {
-    res.status(400).json({ error: "Comment text is required." });
+router.post("/", async (req, res) => {
+  try {
+    const { text, userId } = req.body;
+
+    if (!text) {
+      return res.status(400).json({ error: "Comment text is required." });
+    }
+    // can find the user by ID
+    const user = await User.findByPk(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found." });
+    }
+    // I am assuming Comment model has an association named "UserId"
+    // need to set up sessions.
+    // SET UP tooLocalDateString() helper in UTILS folder.
+    const newComment = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id, 
+    });
+
+    res.status(200).json(newComment);
+  } catch (error) {
+    console.error("Error creating comment:", error);
+    res.status(500).json({ error: "Error creating comment." });
   }
 });
+
 
 // Route to get a specific comment by ID
-router.get("/:id", (req, res) => {
-  const commentId = parseInt(req.params.id);
-  const comment = comments.find(comment => comment.id === commentId);
-  if (comment) {
-    res.json(comment);
-  } else {
-    res.status(404).json({ error: "Comment not found." });
+router.get("/:id", async (req, res) => {
+  try {
+    const comment = await Comment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    if (comment) {
+      res.status(200).json(comment);
+    } else {
+      res.status(404).json({ error: "Comment not found." });
+    }
+  } catch (error) {
+    console.error("Error fetching comment:", error);
+    res.status(500).json({ error: "Error fetching comment." });
   }
 });
+
 
 // Route to update a specific comment by ID
-router.put("/:id", (req, res) => {
-  const commentId = parseInt(req.params.id);
-  const updatedText = req.body.text;
-  const comment = comments.find(comment => comment.id === commentId);
-  if (comment) {
-    comment.text = updatedText;
-    res.json(comment);
-  } else {
-    res.status(404).json({ error: "Comment not found." });
-  }
-});
+// router.put("/:id", async (req, res) => {
+//   try {
+//     const commentId = parseInt(req.params.id);
+//     const updatedText = req.body.text;
+
+//     const comment = await Comment.findByPk(commentId);
+
+//     if (comment) {
+//       comment.text = updatedText;
+//       await comment.save();
+
+//       res.json(comment);
+//     } else {
+//       res.status(404).json({ error: "Comment not found." });
+//     }
+//   } catch (error) {
+//     console.error("Error updating comment:", error);
+//     res.status(500).json({ error: "Error updating comment." });
+//   }
+// });
+
 
 // Route to delete a specific comment by ID
-router.delete("/:id", (req, res) => {
-  const commentId = parseInt(req.params.id);
-  const index = comments.findIndex(comment => comment.id === commentId);
-  if (index !== -1) {
-    comments.splice(index, 1);
-    res.status(204).send();
-  } else {
-    res.status(404).json({ error: "Comment not found." });
+router.delete("/:id", async (req, res) => {
+  try {
+    const comment = await Comment.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+    
+    if (!comment) {
+      res.status(404).json({ error: "Comment not found." });
+      return;
+    } 
+  } catch (error) {
+    console.error("Error deleting comment:", error);
+    res.status(500).json({ error: "Error deleting comment." });
   }
 });
 
