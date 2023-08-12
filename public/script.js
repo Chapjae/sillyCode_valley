84fd8095b40c80467bf7bf997f9741a145c46e77
Jay Liu <jayliu89@gmail.com>
2023-08-11 19:32:40 -0700

    branch features on pause until further notice. will switch branches and work on other features

--- public/script.js
+++ public/script.js
@@ -1,10 +1,35 @@
 // pass the path you want to call
 const socket = io('/room');
+const videoGrid = document.getElementById("video-grid")
+const myPeer = new Peer(undefined, {
+  host: "/room",
+  port: "3001"
+});
 
+const myVideo = document.createElement('video')
+myVideo.muted = true
+
+navigator.mediaDevices.getUserMedia({
+  video:true, 
+  audio:true
+}).then(stream => {
+  addVideoStream(myVideo, stream)
+} )
 // send an event to the room
 // last parameter is user id, hardcoded to test 
-socket.emit("join-room", roomId, 10);
+myPeer.on("open", id => {
+  socket.emit("join-room", roomId, id)
+})
+
 
 socket.on("user-connected", userId => {
   console.log("user connected " + userId)
 })
+
+function addVideoStream(video, stream) {
+  video.srcOrbject= stream
+  video.addEventListener("laodedmetadata", () => {
+    video.play()
+  })
+  videoGrid.append(video)
+}
