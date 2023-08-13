06e3f0b8db3a869666cd53684dcc09ae5c96958a
Jay Liu <jayliu89@gmail.com>
2023-08-12 17:54:55 -0700

    more funtionality added to script.js

--- public/script.js
+++ public/script.js
@@ -1,10 +1,64 @@
+// user's current stream
 let localStream;
+// other users's stream
 let remoteStream;
+// creates the initial peer connection, this is the core interface that allows us to connect to other users
+let peerConnection;
 
+// setup two stun servers to be passed into the new RTCPeerConnection()
+const servers = {
+  
+  iceServers: [
+    {
+      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"]
+    }
+  ]
+}
+
+// function that will ask for permission to use user's camera and audio
 let init = async () => {
   localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
 
-  document.getElementById("localVideo").srcObject = localStream
+  document.getElementById("user-1").srcObject = localStream
+}
+
+
+let createOffer = async () => {
+  peerConnection = new RTCPeerConnection(servers)
+
+  // sets up a connection with other user
+  remoteStream = new MediaStream()
+  document.getElementById("user-2").srcObject = remoteStream
+
+  // get all audio and video tracks from local stream, loop over them and
+  localStream.getTracks().forEach((track) => { 
+  // add looped tracks to the peer connection so that peers can hear and see us 
+    peerConnection.addTrack(track, localStream)
+  });
+
+// listen using an eventlistener for peer tracks so that we can hear / see them
+  peerConnection.ontrack = (event) => {
+    // very similar functionality as line 34, loop over every track from the peer connection
+    event.streams[0].getTracks().forEach((track) => {
+    // add the looped tracks to the remoteStream we instantiated/declared on line 30
+      remoteStream.addTrack(track)
+    })
+  }
+
+  // incecandidtate is an event listener that will create an ICE candidate
+  peerConnection.onicecandidate = async (event) => {
+    if(event.candidate) {
+      console.log("new ICE candadite:", event.candidate)
+    }
+  }
+  // each connection has an offer and a connection
+  let offer = await peerConnection.createOffer()
+
+  // then we set the local description, once set it will trigger line 49 ICE candidates to make 
+  // making requests to the STUN servers, and that will create the ICE candidatese
+  await peerConnection.setLocalDescription(offer)
+
+  console.log("offer:", offer)
 }
 
 init()
--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 const express = require("express");
-const app = express();
 const http = require("http");
-const server = http.createServer(app);
 const socket = require("socket.io");
-const io =  socket(server)
 const exphbs = require("express-handlebars");
 const path = require("path");
 const session = require("express-session");
 const routes = require("./controllers");
-const {v4: uuidv4} = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 const helpers = require("./utils/helpers");
 
 // const sequelize = require("./config/connection");
 // const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const app = express();
 
 // const PORT = process.env.PORT || 3000
-
+const server = http.createServer(app);
+const io = socket(server)
 const hbs = exphbs.create({ helpers });
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "./views");
+
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
 
 // const sess = {
 //   secret: process.env.SECRET_KEY,
@@ -35,54 +42,26 @@ const hbs = exphbs.create({ helpers });
 // io.on('connection', connection);
 // app.use(session(sess));
 
-// app.get("/room", (req, res) => {
-//   res.redirect(`/4`)
-// })
-
-const rooms = {};
-
-io.on('connection', socket => {
-  socket.on('join-room', (roomID) => {
-    if(rooms[roomID]) {
-      rooms[roomID].push(socket.id);
-    } else {
-      rooms[roomID] = [socket.id];
-    }
-    const otherUser = rooms[roomID].find(id => id !== socket.id);
-    if (otherUser) {
-      socket.emit("other user", otherUser);
-      socket.to(otherUser).emit("user joined", socket.id);
-    }
-  });
-  
-  socket.on("offer", payload => {
-    io.to(payload.target).emit( "offer", payload);
-  });
-  
-  socket.on("answer", payload => {
-    io.to(payload.target).emit("answer", payload);
-  });
-
-  socket.on("ice-candadite", incoming => {
-    io.to(incoming.target).emit("ice-candidate", incoming-candidate);
-  });
-  
-});
-
 app.get("/room", (req, res) => {
+  console.log({ req, res })
   res.render("room")
 });
 
-app.engine("hbs", hbs.engine);
-app.set("view engine", "hbs");
-app.set("views", "./views");
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(routes);
 
 // sequelize.sync({ force: false }).then(() => {
 //   app.listen(PORT, () => console.log('Now listening'));
+// });
+
+// socket.on("offer", payload => {
+//   io.to(payload.target).emit( "offer", payload);
+// });
+
+// socket.on("answer", payload => {
+//   io.to(payload.target).emit("answer", payload);
+// });
+
+// socket.on("ice-candadite", incoming => {
+//   io.to(incoming.target).emit("ice-candidate", incoming-candidate);
 // });
 
 
