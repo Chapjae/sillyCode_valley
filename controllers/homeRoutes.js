81dff075ef28711dce0c0d6110410f3663794a1b
Andy Godoy <Andygodoy@ymail.com>
2023-08-13 19:22:46 -0700

    added videos to Scroll feat and added view comment anchor under first video for testing:

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,32 +7,6 @@ const { Video, User } = require('../models');
 // const io = require("socket.io")(server)
 // const {v4: uuidv4} = require("uuid")
 
-// const storeVideoData = async () => {
-//   try {
-//     const options = {
-//       resource_type: 'video'
-//     };
-//     const result = await cloudinary.api.resources(options);
-//     // Extract URLs and created_at keys from the result for videos
-//     const videoInfo = result.resources
-//       .filter(resource => resource.resource_type === 'video')
-//       .map(resource => {
-//         return {
-//           link: resource.url,
-//           created_on: resource.created_at
-//         };
-//       });
-//     // Use Sequelize to store the video data in the database
-//     // await sequelize.sync(); // Sync the database schema if needed
-//     for (const video of videoInfo) {
-//       await Video.create(video);
-//     }
-//     console.log('Video data stored successfully.');
-//   } catch (error) {
-//     console.error('Error storing video data:', error);
-//   }
-// };
-// storeVideoData();
 // const httpServer = createServer();
 
 // router.get("/testing", (req, res) => {
