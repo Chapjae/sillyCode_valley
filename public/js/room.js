db04cda6008ef732613fdea7d3ecf1b548fd15c9
Jay Liu <jayliu89@gmail.com>
2023-08-15 09:55:00 -0700

    added more characters to character array, changed html for room page li elements

--- public/js/room.js
+++ public/js/room.js
@@ -10,8 +10,8 @@ document.getElementById("prompt").addEventListener("click", function(){
  const events = ["Ordering a pizza", "Giving an inspirational speech", "Reviewing a movie", "Hosting a nature show", "Running from a wild animal", 
                 "Anchoring for evening news","Trying to sell a pen","Officiating a funeral", "Looking for a bathroom", "Singing a song", "Floating in space", "Hosting a cooking show", "Officiating a wedding", 
                 "Doing a pre-fight interview", "Programming your second project", "Selling a used car", "Returning clothes that are too small"]
-  const characters = ["Jerome Chenette","Foghorn Leghorn", "Gollum", "Bernie Sanders", "An Auctioneer", "Arnold Schwarzenagger", "Kermit the Frog", "Jim Carey", "Hank Hill", "A knight", "Mike Tyson", "A waiter", "Count Dracula",
-                "Dr. Evil", "Austin Powers", " A radio DJ", "Ruby Rhod", "Sylvester Stallone", "Batman", "A snooty person", "A news anchor", "A priest"]
+  const characters = ["Jerome Chenette","Foghorn Leghorn", "Yoda", "Gollum", "Bernie Sanders", "An auctioneer","Mickey Mouse", "Arnold Schwarzenagger", "Kermit the Frog", "Jim Carey", "Emperor Palpatine", "Hank Hill", "A knight", "Mike Tyson", "A waiter", "Count Dracula",
+                "Dr. Evil", "Austin Powers", " A radio DJ","Darth Vader", "Ruby Rhod", "Sylvester Stallone", "Batman", "A Fashionista","A snooty person", "A news anchor", "A priest"]
 
   let randomEvent = events[Math.floor(Math.random() * events.length)]
   let randomCharacter = characters[Math.floor(Math.random() * characters.length)]
@@ -78,10 +78,6 @@ function startRecording(stream, lengthInMS) {
   return Promise.all([stopped, recorded]).then(() => data);
 }
 
-// function stop(stream) {
- 
-// }
-
 startButton.addEventListener(
   "click",
   () => {
@@ -129,113 +125,3 @@ stopButton.addEventListener(
   },
   false,
 );
-
-
-// const canvas = document.querySelector("canvas");
-
-// // Optional frames per second argument.
-// const stream = canvas.captureStream(25);
-// const recordedChunks = [];
-
-// console.log(stream);
-// const options = { mimeType: "video/webm; codecs=vp9" };
-// const mediaRecorder = new MediaRecorder(stream, options);
-
-// mediaRecorder.ondataavailable = handleDataAvailable;
-// mediaRecorder.start();
-
-// function handleDataAvailable(event) {
-//   console.log("data-available");
-//   if (event.data.size > 0) {
-//     recordedChunks.push(event.data);
-//     console.log(recordedChunks);
-//     download();
-//   } else {
-//     // …
-//   }
-// }
-// function download() {
-//   const blob = new Blob(recordedChunks, {
-//     type: "video/webm",
-//   });
-//   const url = URL.createObjectURL(blob);
-//   const a = document.createElement("a");
-//   document.body.appendChild(a);
-//   a.style = "display: none";
-//   a.href = url;
-//   a.download = "test.webm";
-//   a.click();
-//   window.URL.revokeObjectURL(url);
-// }
-
-// // demo: to download after 9sec
-// setTimeout((event) => {
-//   console.log("stopping");
-//   mediaRecorder.stop();
-// }, 9000);
-
-
-// user's current stream
-// let localStream;
-// // other users's stream
-// let remoteStream;
-// // creates the initial peer connection, this is the core interface that allows us to connect to other users
-// let peerConnection;
-
-// // setup two stun servers to be passed into the new RTCPeerConnection()
-// const servers = {
-  
-//   iceServers: [
-//     {
-//       urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"]
-//     }
-//   ]
-// }
-
-// function that will ask for permission to use user's camera and audio
-// let init = async () => {
-//   localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
-
-//   document.getElementById("user-1").srcObject = localStream
-// }
-
-
-// let createOffer = async () => {
-//   peerConnection = new RTCPeerConnection(servers)
-
-//   // sets up a connection with other user
-//   remoteStream = new MediaStream()
-//   document.getElementById("user-2").srcObject = remoteStream
-
-//   // get all audio and video tracks from local stream, loop over them and
-//   localStream.getTracks().forEach((track) => { 
-//   // add looped tracks to the peer connection so that peers can hear and see us 
-//     peerConnection.addTrack(track, localStream)
-//   });
-
-// // listen using an eventlistener for peer tracks so that we can hear / see them
-//   peerConnection.ontrack = (event) => {
-//     // very similar functionality as line 34, loop over every track from the peer connection
-//     event.streams[0].getTracks().forEach((track) => {
-//     // add the looped tracks to the remoteStream we instantiated/declared on line 30
-//       remoteStream.addTrack(track)
-//     })
-//   }
-
-//   // incecandidtate is an event listener that will create an ICE candidate
-//   peerConnection.onicecandidate = async (event) => {
-//     if(event.candidate) {
-//       console.log("new ICE candadite:", event.candidate)
-//     }
-//   }
-//   // each connection has an offer and a connection
-//   let offer = await peerConnection.createOffer()
-
-//   // then we set the local description, once set it will trigger line 49 ICE candidates to make 
-//   // making requests to the STUN servers, and that will create the ICE candidatese
-//   await peerConnection.setLocalDescription(offer)
-
-//   console.log("offer:", offer)
-// }
-
-// init()
