1f8139a3d043221e694e5163d9ab1ef7fc871708
Andy Godoy <Andygodoy@ymail.com>
2023-08-14 17:55:00 -0700

    added logout/login functions

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,7 +4,7 @@ const videoRoutes = require("./videoRoutes");
 const commentRoutes = require("./commentRoutes");
 
 router.use("/users", userRoutes);
-// http://localhost:3000/api/user
+// http://localhost:3000/api/users
 router.use("/videos", videoRoutes);
 // http://localhost:3000/api/videos
 router.use("/commentRoutes", commentRoutes);
--- public/js/login.js
+++ public/js/login.js
@@ -19,9 +19,32 @@ document.getElementById("sign-up-btn").addEventListener("click", async () => {
         body: JSON.stringify(requestBody),
         headers: { 'Content-Type': 'application/json' },
     })
-    if(response.ok){ 
-        document.location.replace('/');
-    }else {
+    if (response.ok) {
+        document.location.replace('/login');
+    } else {
         console.log('Error Signing up! ')
     }
 })
+
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+    console.log('Hi');
+    // Collect values from the login form
+    const userName = document.querySelector('#username').value.trim();
+    const password = document.querySelector('#password').value.trim();
+    if (email && password) {
+        // Send a POST request to the API endpoint
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ userName, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        if (response.ok) {
+            // If successful, redirect the browser to the profile page
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+document.getElementById('login-btn').addEventListener('click', loginFormHandler);
